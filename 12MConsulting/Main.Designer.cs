4a0ad0a50e07a75de550580541d9137ce30a729a
hakankalkan <kalkan.hakan@outlook.com.tr>
2021-03-10 21:38:34 +0300

    commit

---
+++ 12MConsulting/Main.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace _12MConsulting
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.export = new System.Windows.Forms.Button();
+            this.yazdir = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(262, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(774, 426);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mal Kodu Seçin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Başlangıç Tarihi Girin:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bitiş Tarihi Girin:";
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(152, 279);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(75, 23);
+            this.btnListele.TabIndex = 7;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(106, 76);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(15, 141);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(212, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(15, 203);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(212, 20);
+            this.dateTimePicker2.TabIndex = 10;
+            // 
+            // export
+            // 
+            this.export.Location = new System.Drawing.Point(15, 414);
+            this.export.Name = "export";
+            this.export.Size = new System.Drawing.Size(75, 23);
+            this.export.TabIndex = 11;
+            this.export.Text = "Export";
+            this.export.UseVisualStyleBackColor = true;
+            this.export.Click += new System.EventHandler(this.export_Click);
+            // 
+            // yazdir
+            // 
+            this.yazdir.Location = new System.Drawing.Point(172, 414);
+            this.yazdir.Name = "yazdir";
+            this.yazdir.Size = new System.Drawing.Size(75, 23);
+            this.yazdir.TabIndex = 12;
+            this.yazdir.Text = "Yazdır";
+            this.yazdir.UseVisualStyleBackColor = true;
+            this.yazdir.Click += new System.EventHandler(this.yazdir_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.PrintPage);
+            // 
+            // Main
+            // 
+            this.ClientSize = new System.Drawing.Size(1048, 455);
+            this.Controls.Add(this.yazdir);
+            this.Controls.Add(this.export);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnListele;
+
+        
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button export;
+        private System.Windows.Forms.Button yazdir;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ 12MConsulting/Main.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Office.Interop.Excel;
+using System.Drawing;
+
+namespace _12MConsulting
+{
+    public partial class Main : Form
+    {
+        public static SqlConnection connection = new SqlConnection("server = DESKTOP-MNAVLBM\\SQLEXPRESS;Initial Catalog = test;Integrated Security=True;");
+        
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            
+            FillComboBox();
+
+        }
+        private void FillComboBox()
+        {
+            connection.Open();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT MalKodu FROM STK",connection);
+            System.Data.DataTable dt = new System.Data.DataTable();
+           
+            da.Fill(dt);
+           
+            foreach(DataRow dr in dt.Rows)
+            {
+                comboBox2.Items.Add(dr["MalKodu"].ToString());
+            }
+            connection.Close();
+        }
+
+        public static int Stok;
+        private void Display(string MalKodu, int baslangicTarihi, int bitisTarihi)
+        {
+            try {
+                connection = new SqlConnection("server = DESKTOP-MNAVLBM\\SQLEXPRESS;Initial Catalog = test;Integrated Security=True;");
+                connection.Open();
+                string DropTable = "drop table ViewResult";
+                SqlCommand cmd2 = new SqlCommand(DropTable, connection);
+                cmd2.ExecuteNonQuery();
+                string CreateTable = "if not exists(select * from sysobjects where name = 'ViewResult' and xtype = 'U')    create table ViewResult(SiraNo INT, IslemTur VARCHAR(10), EvrakNo VARCHAR(50), Tarih VARCHAR(16), GirisMiktar VARCHAR(10), CikisMiktar VARCHAR(10), Stok VARCHAR(10))";
+                string selectQuery = "SELECT IslemTur, EvrakNo, Tarih, Miktar FROM STI WHERE @MalKodu=MalKodu AND @baslangicTarihi<=Tarih AND @bitisTarihi>Tarih ORDER BY Tarih";
+                SqlCommand command = new SqlCommand(selectQuery, connection);
+                command.Parameters.AddWithValue("@MalKodu", MalKodu);
+                command.Parameters.AddWithValue("@baslangicTarihi", baslangicTarihi);
+                command.Parameters.AddWithValue("@bitisTarihi", bitisTarihi);
+                SqlCommand cmd = new SqlCommand(CreateTable, connection);
+                
+                cmd.ExecuteNonQuery();
+                command.ExecuteNonQuery();
+                
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = command;
+                System.Data.DataTable ds = new System.Data.DataTable();
+                da.Fill(ds);
+                int i = 1;
+                foreach(DataRow dr in ds.Rows)
+                {
+                    if(dr["IslemTur"].ToString() == "0")
+                    {
+                        string IslemTur = "Giriş";
+                        string EvrakNo = dr["EvrakNo"].ToString();
+                        int tarih = Convert.ToInt32(dr["Tarih"]);
+                        int a = Convert.ToInt32(dr["Miktar"]);
+                        Stok += a;
+                        string CikisMiktar = "0";
+                        string GirisMiktar = a.ToString();
+                        string InsertColoumnValues = "INSERT INTO ViewResult (SiraNo,IslemTur,EvrakNo, Tarih, GirisMiktar, CikisMiktar, Stok) Values(@SiraNo, @IslemTur,@EvrakNo,CONVERT(VARCHAR(15), CAST(@Tarih - 2 AS datetime), 104), @GirisMiktar , @CikisMiktar, @Stok)";
+                        SqlCommand command2 = new SqlCommand(InsertColoumnValues, connection);
+                        command2.Parameters.AddWithValue("@Stok", Stok.ToString());
+                        command2.Parameters.AddWithValue("@GirisMiktar", GirisMiktar);
+                        command2.Parameters.AddWithValue("@CikisMiktar", CikisMiktar);
+                        command2.Parameters.AddWithValue("@Tarih", tarih);
+                        command2.Parameters.AddWithValue("@EvrakNo", EvrakNo);
+                        command2.Parameters.AddWithValue("@IslemTur", IslemTur);
+                        command2.Parameters.AddWithValue("@SiraNo", i.ToString());
+                        i++;
+                        command2.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        string IslemTur = "Çıkış";
+                        string EvrakNo = dr["EvrakNo"].ToString();
+                        int tarih = Convert.ToInt32(dr["Tarih"]);
+                        int a = Convert.ToInt32(dr["Miktar"]);
+                        Stok -= a;
+                        string CikisMiktar = a.ToString();
+                        string GirisMiktar = "0";
+                        string AlterColoumnValues = "INSERT INTO ViewResult (SiraNo, IslemTur, EvrakNo,Tarih, CikisMiktar, GirisMiktar, Stok) Values(@SiraNo, @IslemTur, @EvrakNo,CONVERT(VARCHAR(15), CAST(@Tarih - 2 AS datetime), 104), @CikisMiktar, @GirisMiktar, @Stok)";
+                        SqlCommand command3 = new SqlCommand(AlterColoumnValues, connection);
+                        command3.Parameters.AddWithValue("@CikisMiktar", CikisMiktar);
+                        command3.Parameters.AddWithValue("@GirisMiktar", GirisMiktar);
+                        command3.Parameters.AddWithValue("@Stok", Stok.ToString());
+                        command3.Parameters.AddWithValue("@Tarih", tarih);
+                        command3.Parameters.AddWithValue("@EvrakNo", EvrakNo);
+                        command3.Parameters.AddWithValue("@IslemTur", IslemTur);
+                        command3.Parameters.AddWithValue("@SiraNo", i.ToString());
+                        i++;
+                        command3.ExecuteNonQuery();
+                    }
+                }
+                Stok = 0;
+                string ViewResult = "SELECT * FROM ViewResult";
+                SqlCommand resultCommand = new SqlCommand(ViewResult, connection);
+                SqlDataAdapter d = new SqlDataAdapter();
+                d.SelectCommand = resultCommand;
+                System.Data.DataTable dt = new System.Data.DataTable();
+                d.Fill(dt);
+                dataGridView1.DataSource = dt;
+                connection.Close();
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            GetSelectedValue();
+        }
+
+        private void GetSelectedValue()
+        {
+            string MalKodu = comboBox2.SelectedItem.ToString();
+            DateTime dtBaslangic = new DateTime();
+            dtBaslangic = dateTimePicker1.Value.Date;
+            DateTime dtBitis = new DateTime();
+            dtBitis = dateTimePicker2.Value.Date;
+            int baslangicTarihi = Convert.ToInt32(dtBaslangic.ToOADate());
+            int bitisTarihi = Convert.ToInt32(dtBitis.ToOADate());
+            Display(MalKodu, baslangicTarihi, bitisTarihi);
+        }
+
+        private void export_Click(object sender, EventArgs e)
+        {
+            _Application app = new Microsoft.Office.Interop.Excel.Application();
+            _Workbook workbook = app.Workbooks.Add(Type.Missing);
+            _Worksheet worksheet = null;
+            app.Visible = false; 
+            worksheet = workbook.Sheets["Sayfa1"];
+            worksheet = workbook.ActiveSheet;
+            worksheet.Name = "Exported from gridview";
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+            
+            string spreadsheetName = "Export";
+            app.DisplayAlerts = false;
+            Dialog saveAsDialog = app.Dialogs[XlBuiltInDialog.xlDialogSaveAs];
+            saveAsDialog.Show(spreadsheetName);
+
+            workbook.Close(true);
+            app.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+        }
+        Bitmap bitmap;
+        private void yazdir_Click(object sender, EventArgs e)
+        {
+            int height = dataGridView1.Height;
+            dataGridView1.Height = dataGridView1.RowCount * dataGridView1.RowTemplate.Height * 2;
+            bitmap = new Bitmap(dataGridView1.Width, dataGridView1.Height);
+            dataGridView1.DrawToBitmap(bitmap, new System.Drawing.Rectangle(0, 0, dataGridView1.Width, dataGridView1.Height));
+            dataGridView1.Height = height;
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+        }
+        private void PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(bitmap,0,0);
+        }
+    }
+}
